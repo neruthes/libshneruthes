4786369c295b349d5c5357d9b75612d8f74373e3
Neruthes <i@neruthes.xyz>
2025-07-28 01:14:19 +0000

    Regular update '2025-07-28T01:14:19+00:00'

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### This repo is a public mirror. Should import from NDevShellRC/bin from time to time...
+
+
+
+srcdir="$HOME/DEV/NDevShellRC/bin"
+dstdir="$HOME/DEV/libshneruthes/bin"
+
+if [[ -d "$srcdir" ]] && [[ -d "$dstdir" ]]; then
+    rsync -auvpx --dry-run "$srcdir/" "$dstdir/"
+    echo "Feeling safe? Run the following command to make it happen:"
+    echo rsync -auvpx "$srcdir/" "$dstdir/"
+fi
